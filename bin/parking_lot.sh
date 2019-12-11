15196503e92d45cb492e28b7aaab264928447bcc
sachgrover <sachingrover2@gmail.com>
2019-12-11 11:15:56 +0530

    script maven changes to compile java project

--- bin/parking_lot.sh
+++ bin/parking_lot.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 arg1=$1
-##directory where jar file is located    
-dir=target
-##jar file name
+#directory where jar file is located    
+
+#jar file name
 jar_name=parkinglot-0.0.1-SNAPSHOT.jar
 
 #go back to directory where pom.xml is present to compile project
 cd ..
 
+# compile
+mvn clean install
 
-# maven exec plugin compile and run java main application
+# maven exec plugin to run java main application
 mvn exec:java
